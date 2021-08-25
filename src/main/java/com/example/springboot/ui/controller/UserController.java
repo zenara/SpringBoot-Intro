fb7387410fa3831765b624815d3f2a0855247d99
Chathurika Senani <zenaronline@gmail.com>
2021-08-25 19:26:12 +0530

    Reading HTTP Post Request

--- src/main/java/com/example/springboot/ui/controller/UserController.java
+++ src/main/java/com/example/springboot/ui/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.example.springboot.ui.controller;
 
+import com.example.springboot.ui.model.request.UserDetailsRequestModel;
 import com.example.springboot.ui.model.response.UserRest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,21 +28,38 @@ public class UserController {
         return "get user was called with page = " + page + " limit = " + limit + " sort = " + sort;
     }
 
-    @GetMapping(path="/{userId}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
-    public UserRest getUser(@PathVariable String userId)
+    @GetMapping(path="/{userId}", produces = { 
+        MediaType.APPLICATION_XML_VALUE, 
+        MediaType.APPLICATION_JSON_VALUE
+    })
+    public ResponseEntity<UserRest> getUser(@PathVariable String userId)
     {
         UserRest returnValue = new UserRest();
         returnValue.setFirstName("Chathurika");
         returnValue.setLastName("Jayathilake");
         returnValue.setEmail("test@gmail.com");
         returnValue.setUserId(userId);
-        return returnValue;
+
+        return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
     }
 
-    @PostMapping()
-    public String createUser(@PathVariable String userId)
+    @PostMapping(produces = { 
+        MediaType.APPLICATION_JSON_VALUE,
+        MediaType.APPLICATION_XML_VALUE
+    },
+    consumes = {
+        MediaType.APPLICATION_JSON_VALUE,
+        MediaType.APPLICATION_XML_VALUE
+    })
+    public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetails)
     {
-        return "create user was called";
+        UserRest returnValue = new UserRest();
+        returnValue.setFirstName(userDetails.getFirstName());
+        returnValue.setLastName(userDetails.getLastName());
+        returnValue.setEmail(userDetails.getEmail());
+        returnValue.setUserId(userDetails.getUserId());
+
+        return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
     }
 
     @PutMapping
---
+++ src/main/java/com/example/springboot/ui/model/request/UserDetailsRequestModel.java
@@ -0,0 +1,33 @@
+package com.example.springboot.ui.model.request;
+
+public class UserDetailsRequestModel {
+    private String firstName;
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    private String lastName;
+    private String email;
+    private String userId;
+    
+}
