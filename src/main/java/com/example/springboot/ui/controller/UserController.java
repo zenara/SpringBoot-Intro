df8f95d8c55a6373fd5a0fbc443e2eda65db6aa9
Chathurika Senani <zenaronline@gmail.com>
2021-08-25 17:06:40 +0530

    GET - Pagination

--- src/main/java/com/example/springboot/ui/controller/UserController.java
+++ src/main/java/com/example/springboot/ui/controller/UserController.java
@@ -2,9 +2,11 @@ package com.example.springboot.ui.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,13 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping
-    public String getUser()
+    public String getUsers(@RequestParam(value = "page") int page,
+    @RequestParam(value = "limit") int limit)
     {
-        return "get user was called";
+        return "get user was called with page = " + page + "limit =" + limit;
     }
 
-    @PostMapping
-    public String createUser()
+    @GetMapping(path="/{userId}")
+    public String getUser(@PathVariable String userId)
+    {
+        return "create user was called with user ID = " + userId;
+    }
+
+    @PostMapping()
+    public String createUser(@PathVariable String userId)
     {
         return "create user was called";
     }
